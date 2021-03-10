910065e0df97c86abab4164444eb5d523ef62458
davidbryst <davidbryster17@gmail.com>
2021-03-10 16:00:33 +0000

    componets 1

--- lib/components/Kebygraphique.dart
+++ lib/components/Kebygraphique.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kebyras/constantes.dart';
-// import 'package:fl_chart/fl_chart.dart';
-
-class KebyrasGraphique extends StatelessWidget {
+import 'package:fl_chart/fl_chart.dart';
+/*
+class  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,5 +12,211 @@ class KebyrasGraphique extends StatelessWidget {
         gradient: color_baner,
       ),
     );
+  }
+}*/
+
+class KebyrasGraphique extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => KebyrasGraphiqueState();
+}
+
+class KebyrasGraphiqueState extends State<KebyrasGraphique> {
+  final Color inBarColor = Colors.white70;
+  final Color outBarColor = Colors.black87;
+  final double width = 10;
+
+  List<BarChartGroupData> rawBarGroups;
+  List<BarChartGroupData> showingBarGroups;
+
+  int touchedGroupIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    final barGroup1 = makeGroupData(0, 5, 12);
+    final barGroup2 = makeGroupData(1, 16, 12);
+    final barGroup3 = makeGroupData(2, 18, 5);
+    final barGroup4 = makeGroupData(3, 20, 16);
+    final barGroup5 = makeGroupData(4, 17, 6);
+    final barGroup6 = makeGroupData(5, 19, 1.5);
+    final barGroup7 = makeGroupData(6, 10, 1.5);
+
+    final items = [
+      barGroup1,
+      barGroup2,
+      barGroup3,
+      barGroup4,
+      barGroup5,
+      barGroup6,
+      barGroup7,
+    ];
+
+    rawBarGroups = items;
+
+    showingBarGroups = rawBarGroups;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(20),
+              bottomLeft: Radius.circular(20)),
+          gradient: color_baner,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              const SizedBox(
+                height: 38,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: buildBarChart(), // bulde ***************************
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  BarChart buildBarChart() {
+    return BarChart(
+      BarChartData(
+        maxY: 20,
+        barTouchData: BarTouchData(
+          touchTooltipData: BarTouchTooltipData(
+            tooltipBgColor: Colors.grey,
+            getTooltipItem: (_a, _b, _c, _d) => null,
+          ),
+          touchCallback: (response) {
+            if (response.spot == null) {
+              setState(() {
+                touchedGroupIndex = -1;
+                showingBarGroups = List.of(rawBarGroups);
+              });
+              return;
+            }
+
+            touchedGroupIndex = response.spot.touchedBarGroupIndex;
+
+            setState(
+              () {
+                if (response.touchInput is FlLongPressEnd ||
+                    response.touchInput is FlPanEnd) {
+                  touchedGroupIndex = -1;
+                  showingBarGroups = List.of(rawBarGroups);
+                } else {
+                  showingBarGroups = List.of(rawBarGroups);
+                  if (touchedGroupIndex != -1) {
+                    double sum = 0;
+                    for (BarChartRodData rod
+                        in showingBarGroups[touchedGroupIndex].barRods) {
+                      sum += rod.y;
+                    }
+                    final avg = sum /
+                        showingBarGroups[touchedGroupIndex].barRods.length;
+
+                    showingBarGroups[touchedGroupIndex] =
+                        showingBarGroups[touchedGroupIndex].copyWith(
+                      barRods: showingBarGroups[touchedGroupIndex]
+                          .barRods
+                          .map((rod) {
+                        return rod.copyWith(y: avg);
+                      }).toList(),
+                    );
+                  }
+                }
+              },
+            );
+          },
+        ),
+        titlesData: buildFlTitlesData(),
+        borderData: FlBorderData(
+          show: false,
+        ),
+        barGroups: showingBarGroups,
+      ),
+    );
+  }
+
+  // methode to seting Titel of variable
+  FlTitlesData buildFlTitlesData() {
+    return FlTitlesData(
+      show: true,
+      bottomTitles: SideTitles(
+        showTitles: true,
+        getTextStyles: (value) => const TextStyle(
+            color: text_white, fontWeight: FontWeight.bold, fontSize: 14),
+        margin: 20,
+        getTitles: (double value) {
+          switch (value.toInt()) {
+            case 0:
+              return 'Mn';
+            case 1:
+              return 'Te';
+            case 2:
+              return 'Wd';
+            case 3:
+              return 'Tu';
+            case 4:
+              return 'Fr';
+            case 5:
+              return 'St';
+            case 6:
+              return 'Sn';
+            default:
+              return '';
+          }
+        },
+      ),
+      leftTitles: SideTitles(
+        showTitles: true,
+        getTextStyles: (value) => const TextStyle(
+            color: text_white, fontWeight: FontWeight.bold, fontSize: 14),
+        margin: 32,
+        reservedSize: 14,
+        getTitles: (value) {
+          if (value == 0) {
+            return '1K';
+          } else if (value == 10) {
+            return '5K';
+          } else if (value == 19) {
+            return '10K';
+          } else {
+            return '';
+          }
+        },
+      ),
+    );
+  }
+
+  // methode to create make groupe data
+  BarChartGroupData makeGroupData(int x, double y1, double y2) {
+    return BarChartGroupData(barsSpace: 4, x: x, barRods: [
+      BarChartRodData(
+        y: y1,
+        colors: [Colors.white, inBarColor],
+        width: width,
+      ),
+      BarChartRodData(
+        y: y2,
+        colors: [Colors.black, outBarColor],
+        width: width,
+      ),
+    ]);
   }
 }
---
+++ lib/components/KebyrasIcon.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+Widget makeTransactionsIcon() {
+  const double width = 4.5;
+  const double space = 3.5;
+  return Row(
+    mainAxisSize: MainAxisSize.min,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Container(
+        width: width,
+        height: 10,
+        color: Colors.white.withOpacity(0.4),
+      ),
+      const SizedBox(
+        width: space,
+      ),
+      Container(
+        width: width,
+        height: 28,
+        color: Colors.white.withOpacity(0.8),
+      ),
+      const SizedBox(
+        width: space,
+      ),
+      Container(
+        width: width,
+        height: 42,
+        color: Colors.white.withOpacity(1),
+      ),
+      const SizedBox(
+        width: space,
+      ),
+      Container(
+        width: width,
+        height: 28,
+        color: Colors.white.withOpacity(0.8),
+      ),
+      const SizedBox(
+        width: space,
+      ),
+      Container(
+        width: width,
+        height: 10,
+        color: Colors.white.withOpacity(0.4),
+      ),
+    ],
+  );
+}
